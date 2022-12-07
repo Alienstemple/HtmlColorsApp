6926f4c94d56bc8c78a7112cddb7fc4ed0cda9cc
Alyona <borisovsk_a@mail.ru>
2022-12-07 12:35:02 +0300

    feat: RecyclerList first approach

--- app/src/main/java/com/example/htmlcolorsapp/MainActivity.kt
+++ app/src/main/java/com/example/htmlcolorsapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.htmlcolorsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.htmlcolorsapp.databinding.ActivityMainBinding
+import com.example.htmlcolorsapp.databinding.RowItemBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainBinding: ActivityMainBinding
+
+    lateinit var plantRecylerview : RecyclerView
+    lateinit var rowItemBinding: RowItemBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        rowItemBinding = RowItemBinding.inflate(layoutInflater)  // initialize row item with layout
+        mainBinding.apply {
+
+        }
     }
 }
